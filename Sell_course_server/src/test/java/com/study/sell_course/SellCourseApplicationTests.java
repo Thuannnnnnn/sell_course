5f0487360e5a95d2d176961d7f66fb8499a3d883
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-11-23 14:39:38 +0700

    commit

---
+++ Sell_course_server/src/test/java/com/study/sell_course/SellCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.sell_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
