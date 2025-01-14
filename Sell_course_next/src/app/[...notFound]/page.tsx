6e195fba251f265d2fbbb9895021e812f28b2e74
NTPhuc0808 <ntphuc0808@gmail.com>
2025-01-14 22:16:05 +0700

    Update don't use MUI

--- Sell_course_next/src/app/[...notFound]/page.tsx
+++ Sell_course_next/src/app/[...notFound]/page.tsx
@@ -1,21 +1,20 @@
 // NotFound.tsx
 'use client';
 import Link from "next/link";
-import ErrorIcon from "@material-ui/icons/Error";
-import { Typography } from "@material-ui/core";
+import { MdError } from "react-icons/md";
 import './NotFound.css';
 
 const NotFound = () => {
   return (
     <div className="pageNotFound">
       <div className="content">
-        <ErrorIcon className="icon" />
-        <Typography variant="h2" component="h1" className="title">
+        <MdError className="icon" />
+        <h1 className="title">
           Oops! Page Not Found
-        </Typography>
-        <Typography variant="body1" className="description">
+        </h1>
+        <h3 className="description">
           The page you are looking for doesn’t exist or has been moved.
-        </Typography>
+        </h3>
         <Link href="/" className="homeLink">
           Go Back Home
         </Link>
