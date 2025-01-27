bef90e946a90a0cf1b99685549b975d91837912e
Vinhf <thanhvinh1662003@gmail.com>
2025-01-27 07:36:07 +0700

    .

--- Sell_course_next/src/components/category/CourseList.tsx
+++ Sell_course_next/src/components/category/CourseList.tsx
@@ -4,6 +4,7 @@ import { Category } from "@/app/type/category/Category";
 import { deleteCategory } from "@/app/api/category/CategoryAPT";
 import { Container } from "react-bootstrap";
 import { useTranslations } from "next-intl";
+import { useTheme } from "@/contexts/ThemeContext";
 interface CategoryListProps {
   categories: Category[];
   setCategories: React.Dispatch<React.SetStateAction<Category[]>>;
@@ -12,8 +13,7 @@ const CategoryList: React.FC<CategoryListProps> = ({
   categories,
   setCategories,
 }) => {
-
-  const t = useTranslations('categoies')
+  const t = useTranslations("categoies");
   const handleDelete = async (categoryId: string) => {
     try {
       await deleteCategory(categoryId);
@@ -52,7 +52,20 @@ const CategoryList: React.FC<CategoryListProps> = ({
                   onClick={() => console.log("Edit category:", category)}
                   style={{ marginRight: "10px" }}
                 >
-                  ✏️
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    className="bi bi-pencil-square"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                    <path
+                      fill-rule="evenodd"
+                      d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
+                    />
+                  </svg>
                 </button>
                 <button onClick={() => handleDelete(category.categoryId)}>
                   🗑️
