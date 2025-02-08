d903b61ab7e67f2deea6cc666340f1557ad3c298
Thuannnnnnn <tranquocthuan2003@gmail.com>
2025-02-08 17:28:23 +0700

    commit

--- sell_course_nest/src/modules/course_purchase/course_purchase.controller.ts
+++ sell_course_nest/src/modules/course_purchase/course_purchase.controller.ts
@@ -1,35 +1,6 @@
 import {
   Body,
   Controller,
-  HttpException,
-  Post,
-  UseGuards,
-} from '@nestjs/common';
-import { Course_purchaseService } from './course_purchase.service';
-import { JwtAuthGuard } from '../Auth/jwt-auth.guard';
-@Controller('api')
-export class Course_purchaseController {
-  constructor(
-    private readonly coursePurchasedService: Course_purchaseService,
-  ) {}
-  @UseGuards(JwtAuthGuard)
-  @Post('course_purchased/create')
-  async createCoursePurchased(
-    @Body() body: { email: string; courseIds: string[] },
-  ) {
-    const { email, courseIds } = body;
-    if (!email) {
-      throw new HttpException('Bad Request', 400);
-    }
-    if (this.coursePurchasedService.createCoursePurchased(email, courseIds)) {
-      throw new HttpException('OK', 200);
-    }
-    throw new HttpException('Server error', 500);
-  }
-}
-import {
-  Body,
-  Controller,
   Get,
   HttpException,
   Post,
@@ -47,13 +18,13 @@ export class Course_purchaseController {
   @UseGuards(JwtAuthGuard)
   @Post('course_purchased/create')
   async createCoursePurchased(
-    @Body() body: { user_id: string; course_id: string },
+    @Body() body: { email: string; courseIds: string[] },
   ) {
-    const { user_id, course_id } = body;
-    if (!user_id || !course_id) {
+    const { email, courseIds } = body;
+    if (!email) {
       throw new HttpException('Bad Request', 400);
     }
-    if (this.coursePurchasedService.createCoursePurchased(user_id, course_id)) {
+    if (this.coursePurchasedService.createCoursePurchased(email, courseIds)) {
       throw new HttpException('OK', 200);
     }
     throw new HttpException('Server error', 500);
