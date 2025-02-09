181c8b8e6bbd8c1b9eb1b05b042a119f16594cba
Vinhf <thanhvinh1662003@gmail.com>
2025-02-09 21:12:39 +0700

    .

--- Sell_course_next/src/components/course/CourseCard.tsx
+++ Sell_course_next/src/components/course/CourseCard.tsx
@@ -1,5 +1,7 @@
+import { CoursePurchaseAPI } from "@/app/api/coursePurchased/coursePurchased";
 import { Course } from "@/app/type/course/Course";
 import "@/style/CourseCard.css";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useParams, useRouter } from "next/navigation";
 import { AiFillStar, AiOutlineStar } from "react-icons/ai";
@@ -10,7 +12,16 @@ interface CourseCardProps {
 export default function CourseCard({ course }: CourseCardProps) {
   const router = useRouter();
   const params = useParams();
-  const handleClick = () => {
+  const { data: session } = useSession();
+  const email = session?.user.email || "";
+  const handleClick = async () => {
+    const data = await CoursePurchaseAPI.getCoursePurchaseById(
+      course.courseId,
+      email
+    );
+    if(data) {
+      router.push(`/course/${course.courseId}/purchase/${data.id}`);
+    }
     const locale = params.locale;
     router.push(`/${locale}/courseDetail/${course.courseId}`);
   };
