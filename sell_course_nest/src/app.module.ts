d253c027db666fd89acdfe512a2e08d5cb96f209
Thuannnnnnn <tranquocthuan2003@gmail.com>
2025-02-16 19:47:52 +0700

    commit

--- sell_course_nest/src/app.module.ts
+++ sell_course_nest/src/app.module.ts
@@ -79,6 +79,9 @@ import { Order } from './modules/order/entities/order.entity';
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
+      ssl: {
+        rejectUnauthorized: false,
+      },
       entities: [
         Notify,
         UserNotify,
