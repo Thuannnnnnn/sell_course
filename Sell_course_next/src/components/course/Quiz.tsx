f3afd18512c349aa5201cd847acf2b65a1451faa
NguyenMinh993 <nguyenminh090903@gmail.com>
2025-02-20 00:46:47 +0700

    FE courseInfo

---
+++ Sell_course_next/src/components/course/Quiz.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { FaCheckCircle } from "react-icons/fa";
+import { FaQuestionCircle } from "react-icons/fa";
+import { RiArrowDownSLine, RiArrowUpSLine } from "react-icons/ri";
+import "../../style/CourseInfo.css";
+
+const QuizSection = ({ section, index, expandedSection, toggleSection, handleItemClick,completedItems, selectedItem }) => {
+  return (
+    <div className={`section ${expandedSection === index ? "active" : ""}`}>
+      <div className="section-header" onClick={() => toggleSection(index)}>
+        <h4><FaQuestionCircle size={16} /> {section.title}</h4>
+        {expandedSection === index ? <RiArrowUpSLine size={20} /> : <RiArrowDownSLine size={20} />}
+      </div>
+       {expandedSection === index && (
+              <div className="item-list">
+                {section.items.map((item, idx) => {
+                  const key = `${index}-${idx}`;
+                  return (
+                    <div key={idx} className={`item ${selectedItem === item ? "selected" : ""} ${completedItems[key] ? "completed" : ""}`} onClick={() => handleItemClick(index, idx)}>
+                      <span className="item-name">{item.name}</span>
+                      {completedItems[key] ? <FaCheckCircle size={16} color="green" /> : <span>{item.duration}</span>}
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+    </div>
+  );
+};
+
+export { QuizSection };
