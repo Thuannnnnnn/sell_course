5075c5ba89d738a3ab996f6f40fe30f15cbc8b63
0Tung123 <harrybother356@gmail.com>
2025-02-25 00:53:36 +0700

    commit

--- Sell_course_next/src/app/type/quizz/quizz.ts
+++ Sell_course_next/src/app/type/quizz/quizz.ts
@@ -1,20 +1,18 @@
-// quizz.d.ts
 
-// Interface for an Answer
 export interface Answer {
   answerId: string;
   answer: string;
   isCorrect: boolean;
 }
 
-// Interface for a Question
+
 export interface Question {
   questionId: string;
   question: string;
   answers: Answer[];
 }
 
-// Interface for a Quiz
+
 export interface Quiz {
   quizzId: string;
   contentId: string;
@@ -30,7 +28,7 @@ export interface QuizSubmissionData {
   quizzId?: string;
 }
 
-// Interface for creating a new quiz
+
 export interface CreateQuizzDto {
   contentId: string;
   questions: {
@@ -42,7 +40,6 @@ export interface CreateQuizzDto {
   }[];
 }
 
-// Interface for updating an existing quiz
 export interface UpdateQuizzDto {
   quizzId: string;
   questions: {
