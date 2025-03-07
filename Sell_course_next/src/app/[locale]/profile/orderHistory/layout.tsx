df72f30c1f6c50b4e1a1846b150af6c7ded4f748
NTPhuc0808 <ntphuc0808@gmail.com>
2025-03-07 16:13:46 +0700

    done frontend

--- Sell_course_next/src/app/[locale]/profile/orderHistory/layout.tsx
+++ Sell_course_next/src/app/[locale]/profile/orderHistory/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 export const metadata: Metadata = {
-  title: 'Wish List',
-  description: 'WishList Page',
+  title: 'Order History',
+  description: 'OrderHistory Page',
 };
 
 export default function RootLayout({
