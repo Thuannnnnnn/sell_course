49062dc318ad63739b03bf3145d25cc7d00fe639
0Tung123 <harrybother356@gmail.com>
2025-03-31 16:30:01 +0700

    git backup for devok:1743413385342

--- sell_course_nest/src/modules/meeting/meeting.controller.ts
+++ sell_course_nest/src/modules/meeting/meeting.controller.ts
@@ -1,6 +1,3 @@
-// 假设原始代码是一个完整的 TypeScript 文件，以下是移除指定行后的完整代码
-// 注意：由于不知道具体代码内容，假设原始代码是一个标准的 NestJS 控制器文件
-
 import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { MeetingService } from './meeting.service';
 import { CreateMeetingDto } from './dto/create-meeting.dto';
@@ -15,15 +12,11 @@ export class MeetingController {
   }
 
   @Get(':id')
-  async getMeetingById(@Param('id') id: string) {
-    return this.meetingService.getMeetingById(id);
+  async getMeeting(@Param('id') id: string) {
+    return this.meetingService.getMeeting(id);
   }
 
   // Removed sendMessage and getMeetingMessages endpoints
 
-  // 以下是其他可能存在的代码，假设保留了剩余部分
-  @Get()
-  async getAllMeetings() {
-    return this.meetingService.getAllMeetings();
-  }
+  // Removed getAllMeetings endpoint as it's not necessary
 }
