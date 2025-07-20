955bfab78229106a1aa087ec39fc7aae458faa8a
NguyenMinh993 <nguyenminh090903@gmail.com>
2025-07-20 20:12:13 +0700

    done code

--- sell_course_nest/src/modules/certificate/certificate.module.ts
+++ sell_course_nest/src/modules/certificate/certificate.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CertificateController } from './certificate.controller';
+import { CertificateStaticController } from './certificateStatic.controller'; // Add this import
 import { CertificateService } from './certificate.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Certificate } from './entities/certificate.entity';
@@ -7,7 +8,10 @@ import { MailService } from '../../utilities/mail.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Certificate])],
-  controllers: [CertificateController],
+  controllers: [
+    CertificateController, 
+    CertificateStaticController  // Add this controller
+  ],
   providers: [CertificateService, MailService],
   exports: [CertificateService],
 })
